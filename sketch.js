8d444c638aa169cc1e7462e644188334c98a1a50
adriqnb <adrianb62@outlook.com>
2025-11-28 01:00:45 -0500

    f the clouds, changed bounds back to the window size and now the background is a moving ocean YAY

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 let showText = true;
 
 let player;
-let spritePlayer, spriteEnemy, spriteCrosshair, clouds;
+let spritePlayer, spriteEnemy, spriteCrosshair, clouds, bgTile;
 let enemyShips = [];
 let playerBullet = [];
-let timePassed;
+let bgArray = [];
+let lastShotTime;
 let death = false;
 let score = 0;
 let bgVolume = 0.25;
@@ -20,7 +21,7 @@ function preload() {
   spriteEnemy = loadImage('assets/enemy.gif');
   spritePlayer = loadImage('assets/player.gif');
   spriteCrosshair = loadImage('assets/crosshair.gif');
-  clouds = loadImage('assets/clouds.png');
+  bgTile = loadImage('assets/bgTile.png');
 
   shootSound = loadSound('assets/sounds/Pew.wav');
   shootSound.setVolume(bulletVolume);
@@ -38,82 +39,96 @@ function setup() {
   createCanvas(windowWidth,windowHeight);
   angleMode(DEGREES);
   player = new Player();
-  timePassed = millis();
+  lastShotTime = millis();
 }
 
 function draw() {
   background('#0071a7');
+  drawBg();
+  for (let i = 0; i < bgArray.length; i++) {
+    bgArray[i].draw();
+  }
   noCursor();
-
+  
   startMenu();
 
   backgroundMusicPlay();
 
-  if(player.health <= 0)
+  if (player.health <= 0)
     death = true;
   
-  if(death === false){
+  if (death === false){
+    player.checkMovement();
+    player.checkShooting(); //rapid fire function
+    player.update();
+    player.display();
+    for (i=0; i<enemyShips.length; i++){
+      enemyShips[i].applyTanForce(.01);  
+      enemyShips[i].update();  
+      enemyShips[i].display();
+    } 
+
+    for (i=0; i<playerBullet.length; i++){
+      const pb = playerBullet[i];
+      pb.applyTanForce(.1);
+      pb.update();
+      pb.display();
+
+      for (j = 0; j<enemyShips.length; j++){
+        if (playerBullet.length != 0 && checkCollision(pb.pos,enemyShips[j].pos,pb.size,enemyShips[j].rectWidth,enemyShips[j].rectHeight,pb.mainAngle))
+        {
+          enemyShips.splice(j,1);
+          j--;
+          playerBullet.splice(i,1);
+          i--;
+          score += 10;
+        }
+      }
+    }
+    for(i=0; i<enemyBullet.length; i++){
+      enemyBullet[i].applyTanForce(.1);
+      enemyBullet[i].update();
+      enemyBullet[i].display();
+      if(checkCollision(enemyBullet[i].pos,player.pos,enemyBullet[i].size,player.rectWidth,player.rectHeight,player.mainAngle)) {
+        console.log("death");
+        enemyBullet.splice(i,1);
+        i--
+        player.health -= 10;
+      }
+    }
 
     //draw health bar
-    push()
-    fill(0)
-    rect(player.pos.x-35,player.pos.y+35,map(player.maxHealth,0,100,0,70),10); //max health
-    pop()
-    push()
-    fill(41,255,82)
-    rect(player.pos.x-35,player.pos.y+35,map(player.health,0,100,0,70),10); //current health
+    push();
+    shadow('rgba(0, 0, 0, 1)');
     fill(0);
+    rect(player.pos.x-35,player.pos.y+45,map(player.maxHealth,0,100,0,70),13); //max health
+    pop();
+    fill('rgba(0, 192, 35, 1)');
+    rect(player.pos.x-35,player.pos.y+45,map(player.health,0,100,0,70),13); //current health
+    push()
+    shadow('rgba(0, 0, 0, 1)');
+    fill(255);
+    textAlign(CENTER);
     textSize(15);
-    text(player.health+'/'+player.maxHealth,player.pos.x,player.pos.y+60)
-    pop()
-
-  player.checkMovement();
-  player.checkShooting(); //rapid fire function
-  player.update();
-  player.display();
-  for(i=0; i<enemyShips.length; i++){
-    enemyShips[i].applyTanForce(.01);  
-    enemyShips[i].update();  
-    enemyShips[i].display();
-  }
-  for(i=0; i<playerBullet.length; i++){
-    const pb = playerBullet[i];
-    pb.applyTanForce(.1);
-    pb.update();
-    pb.display();
+    text(player.health+'/'+player.maxHealth,player.pos.x,player.pos.y+56.5)
+    pop();
+  } 
   
-    for(j = 0; j<enemyShips.length; j++){
-    if(playerBullet.length != 0 && checkCollision(pb.pos,enemyShips[j].pos,pb.size,enemyShips[j].rectWidth,enemyShips[j].rectHeight,pb.mainAngle))
-    {
-      enemyShips.splice(j,1);
-      j--;
-      playerBullet.splice(i,1);
-      i--;
-      score += 10;
-    }
-    }
-  }
- for(i=0; i<enemyBullet.length; i++){
-    enemyBullet[i].applyTanForce(.1);
-    enemyBullet[i].update();
-    enemyBullet[i].display();
-    if(checkCollision(enemyBullet[i].pos,player.pos,enemyBullet[i].size,player.rectWidth,player.rectHeight,player.mainAngle)) {
-      console.log("death");
-      enemyBullet.splice(i,1);
-      i--
-      player.health -= 10;
-    }
-  }
-
-  drawClouds();
-  }
   if(death === true)
   {
+    push();
+    shadow('rgba(0, 0, 0, 1)');
     fill(255)
+    textSize(40);
+    textAlign(CENTER);
     text("Game Over! Press R to restart", windowWidth/2,windowHeight/2-100)
     text("score: ")
+    pop();
   }
+  push();
+  shadow('rgba(0, 0, 0, 1)');
   image(spriteCrosshair, mouseX-25.5, mouseY-13.5);
+  pop();
   spriteCrosshair.delay(5);
 }
 
@@ -137,6 +152,7 @@ class Player {
   }
   display() {
     push()
+    shadow('rgba(0, 0, 0, 1)');
     translate(this.pos.x,this.pos.y);
     rotate(this.mainAngle -90);
     image(spritePlayer, -32, -32);
@@ -146,40 +162,41 @@ class Player {
   checkMovement()
   {
     //-------------WASD----------------
-    if(keyIsDown(87))
+    if (keyIsDown(87))
       this.vel.y -= .75;
     
-    if(keyIsDown(65)){
+    if (keyIsDown(65)){
       this.vel.x -= .75
     }
-    else if(keyIsDown(68)){
+    else if (keyIsDown(68)){
       this.vel.x +=.75;
     }
-    if(keyIsDown(83))
+
+    if (keyIsDown(83))
       this.vel.y +=.75
     //----------------------------------
     //          Afterburner
 
     // constrain player to window
-    this.pos.x = constrain(this.pos.x, 120, windowWidth-120);
-    this.pos.y = constrain(this.pos.y, 120, windowHeight-120);
+    this.pos.x = constrain(this.pos.x, 35, windowWidth-35);
+    this.pos.y = constrain(this.pos.y, 35, windowHeight-35);
   }
   
   checkShooting()
   {
     //----------------------------------
     //    Check for shooting input
-    if(keyIsDown(32) && (millis() > (timePassed + 150))) // (timePassed + interval between bullets)
+    if(keyIsDown(32) && (millis() > (lastShotTime + 150))) // (lastShotTime + interval between bullets)
     {
       console.log("Ran Bullet");
-      playerBullet.push(new bullet(true));
+      playerBullet.push(new Bullet(true));
 
       spriteCrosshair.reset();
 
       // audio play shoot sound
       shootSound.play();
 
-      timePassed = millis();
+      lastShotTime = millis();
     }
   }
 
@@ -229,6 +246,7 @@ class Enemy{
   }
   display() {
     push()
+    shadow('rgba(0, 0, 0, 1)');
     translate(this.pos.x,this.pos.y);
     rotate(this.mainAngle+270);
     image(spriteEnemy, -32, -32);
@@ -242,7 +260,7 @@ class Enemy{
          
   }
   
-  class bullet
+  class Bullet
   {
   constructor(playerOrEnemy){
     if(playerOrEnemy === true)
@@ -273,6 +291,7 @@ class Enemy{
   {
     push()
     //translate(this.pos.x,this.pos.y);
+    shadow('rgba(0, 0, 0, 1)');
     fill(this.color);
     
 
@@ -309,12 +328,15 @@ function startMenu()
 {
   // start game text
   if (showText) {
+    push();
+    shadow('rgba(0, 0, 0, 1)');
     fill(255);
     textSize(20);
     textAlign(CENTER);
     text("Use WASD to move, mouse to aim, Space to shoot, P to spawn enemy ships", windowWidth/2, 150);
     textSize(40);
     text("Press any key to start", windowWidth/2, windowHeight/2);
+    pop();
   }
 }
 
@@ -331,30 +353,27 @@ function backgroundMusicPlay()
 function runBullet()
 {
   for(i = 0; i<enemyShips.length; i++)
-    enemyBullet.push(new bullet(false));
+    enemyBullet.push(new Bullet(false));
 }
 
-function checkCollision(bulletPos,RectPos,circleSize,rectWidth,rectHeight,angle)
+function checkCollision(bulletPos,rectPos,circleSize,rectWidth,rectHeight,angle)
 {
- circleX = bulletPos.x;
- circleY = bulletPos.y;
- rectX = RectPos.x;
- rectY = RectPos.y;
- circleR = circleSize*2;
- rectW = rectWidth;
- rectH = rectHeight;
+  circleX = bulletPos.x;
+  circleY = bulletPos.y;
+  rectX = rectPos.x;
+  rectY = rectPos.y;
+  circleR = circleSize*2;
+  rectW = rectWidth;
+  rectH = rectHeight;
 
- 
-
-if (
+  if (
     circleX + circleR > rectX && // right edge of circle > left edge of rectangle
     circleX - circleR < rectX + rectW && // left edge of circle < right edge of rectangle
     circleY + circleR > rectY && // bottom edge of circle > top edge of rectangle
     circleY - circleR < rectY + rectH
-  ) {
+  )
+  {
     // top edge of circle < bottom edge of rectangle
-    // collision detected
-    bg = color(0, 0, 255);
     if (circleX + circleR > rectX && circleX < rectX) {
       // circle hit left edge of rectangle
       return true;
@@ -368,9 +387,8 @@ if (
       // circle hit bottom edge of rectangle
       return true;
     }
+
   } else {
-    // no collision
-    bg = color(120, 120, 120);
     collisionSide = "";
   }
 }
@@ -379,32 +397,50 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function drawClouds() {
-  // draw clouds
-  for (let i = 180; i <= windowWidth-180; i += 180) {
-    image(clouds, i - 80, windowHeight - 120, 200, 200);
-    image(clouds, i - 80, -80, 200, 200);
-  }  
-  for (let i = 0; i <= windowHeight; i += 180) {
-    image(clouds, -80, i - 80, 200, 200);
-    image(clouds, windowWidth - 120, i - 50, 200, 200);
-  }  
+//evil overcomplicated bg class and function
+class TiledBg {
+  constructor(initX) {
+    this.timer = 0;
+    this.initX = initX;
+  }
+
+  draw() {
+    this.timer += 0.75;
+    push();
+    translate(this.timer, 0);
+    for (let x = this.initX; x < (windowWidth + this.initX); x += bgTile.width) {
+      for (let y = 0; y < windowHeight; y += bgTile.height) { 
+        image(bgTile, x, y);
+      }
+    }
+    pop();
+  }
+
+  getTimer() {
+    return this.timer;
+  }
+
+  resetTimer() {
+    this.timer = 0;
+  }
+
+}
+
+function drawBg() {
+  if (bgArray.length < 2) {
+    bgArray.push(new TiledBg(0));
+    bgArray.push(new TiledBg(-windowWidth+95));
+  }
+  if(bgArray[0].getTimer() > windowWidth-95) {
+    bgArray[0].resetTimer();
+  }
+  if (bgArray[1].getTimer() > windowWidth-95) {
+    bgArray[1].resetTimer();  
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-
-function drawClouds() {
-  // draw clouds
-  for (let i = 180; i <= windowWidth-180; i += 180) {
-    image(clouds, i - 80, windowHeight - 120, 200, 200);
-    image(clouds, i - 80, -80, 200, 200);
-  }  
-  for (let i = 0; i <= windowHeight; i += 180) {
-    image(clouds, -80, i - 80, 200, 200);
-    image(clouds, windowWidth - 120, i - 50, 200, 200);
-  }  
 }
 
 function resetGame()
@@ -417,4 +453,15 @@ function resetGame()
   enemyShips = [];
   playerBullet = [];
   enemyBullet = [];
+}
+
+function shadow(color, blurRadius = 10, offsetX = 0, offsetY = 0) {
+    drawingContext.shadowColor = color;
+    drawingContext.shadowBlur = blurRadius;
+    drawingContext.shadowOffsetX = offsetX;
+    drawingContext.shadowOffsetY = offsetY;
+}
+
+function noShadow() {
+    drawingContext.shadowColor = '#0000';
 }
