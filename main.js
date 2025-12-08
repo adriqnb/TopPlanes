55e8f514c3a842eed327db6de1a1799815c52810
lmelfa <luke.melfa@stonybrook.edu>
2025-12-07 20:35:30 -0500

    made comments in main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+// Global variables
 let showText = true;
 let showReminder = false;
 let paused = false;
 
+// Game state variables
 let player;
 let spritePlayer, spriteEnemy, spriteCrosshair, dialogBox;
 let pixelFont;
 
-
+// Background variables
 let death = false;
 let score = 0;
 let playerBulletArr = [];
@@ -15,7 +17,7 @@ let bossBulletArr = [];
 let enemyCopters = [];
 let bossCopters = [];
 
-let bulletInterval = setInterval(runBullet, 1000);
+let bulletInterval = setInterval(runBullet, 1000);  // Bullet firing rpm
 
 // Set audio volume
 let bgVolume = 0.12; // Background music
@@ -57,26 +59,31 @@ function setup() {
 }
 
 function draw() {
-  drawBg();
-  noCursor();
+  drawBg(); // Draw background columns
+  noCursor(); // Hide cursor
   
-  startMenu();
+  startMenu();  // Display start menu
 
-  backgroundMusicPlay();
+  backgroundMusicPlay();  // Play background music
 
+  // check for player death
   if (player.health <= 0)
     death = true;
 
+  // Main game loop
   if (!death) {
+    // variables for health pack spawn location
     hpX = enemyCopters.length > 0 ? enemyCopters[0].pos.x : random(50, width - 50);
     hpY = enemyCopters.length > 0 ? enemyCopters[0].pos.y : random(50, height - 50);
 
+    // update and display player
     player.checkMovement();
     player.checkShooting(); //rapid fire function
     player.checkCollision();
     player.update();
     player.display();
 
+    // update and display enemies, bosses, and bullets
     copterArrUpdate(enemyCopters);
     copterArrUpdate(bossCopters);
     bulletArrUpdate(playerBulletArr);
@@ -89,28 +96,32 @@ function draw() {
     text(`${Math.trunc(frameRate())}`, 20, 30);
     pop();
 
-    drawHealthBar();
+    drawHealthBar();  // draw player health bar
 
     // spawn health packs upon enemy kills at latest enemy killed's location
     if (healthScore % 50 === 0 && healthScore != 0 && smallHealthPacks.length < 1) {
-      smallHealthPacks.push(new SmallHealthPack(hpX, hpY));
+      smallHealthPacks.push(new SmallHealthPack(hpX, hpY)); // push new health pack to array
 
+      // reset health pack spawn location
       hpX = enemyCopters.length > 0 ? enemyCopters[0].pos.x : random(50, width - 50);
       hpY = enemyCopters.length > 0 ? enemyCopters[0].pos.y : random(50, height - 50);
     }
 
     // draw health packs
     for (let h = 0; h < smallHealthPacks.length; h++) {
-      smallHealthPacks[h].display();
+      smallHealthPacks[h].display();  // display health pack
+
+      // check for collision with player
       if (isCollision(smallHealthPacks[h].getPos(), player.pos, 12, player.rectWidth, player.rectHeight)) {
-        smallHealthPacks.splice(h, 1);
-        smallHPPickup();
+        smallHealthPacks.splice(h, 1);  // remove health pack from array
+        smallHPPickup();  // apply health pack effect
         h--;
 
         healthScore = 0; // reset health score to prevent multiple spawns
       }
     }
 
+    // display current wave
     push();
     fill(255);
     textSize(40);
@@ -118,24 +129,29 @@ function draw() {
     text("Wave " + wave, width/20, height/10);
     pop();
 
+    // wave management
     if (start) {
       startWave();
+      // spawn enemies
       if ((millis() > (eSpawnTime + 1000-(wave*10))) && ((enemyCount-enemyKills) > (enemyCopters.length))) {
         spawnEnemy();
         eSpawnTime = millis();
       }
 
+      // spawn bosses
       if ((millis() > (bSpawnTime + 1000-(wave*10))) && ((bossCount-bossKills) > (bossCopters.length)) && ((wave % 5) == 0)) {
         spawnBoss();
         bSpawnTime = millis();
       }
 
+      // check for end of wave
       if (((wave % 5) == 0)) {
         if (enemyCount <= enemyKills && bossCount <= bossKills) {
           wave++;
           endWave();
         }
       } else {
+        // regular waves check
         if (enemyCount <= enemyKills) {
           wave++;
           endWave();
@@ -143,7 +159,9 @@ function draw() {
       }
     }
     
+    // power up screen
     if (powerUpScreen) {
+      // if power up not yet chosen, randomize choices and create choice boxes
       if (!powerUpChosen) {
         randomizePowerUp();
         dialog1 = new PowerUpDialog(choice);
@@ -156,14 +174,15 @@ function draw() {
     }
   } 
   
+  // Game over screen
   if (death) {
     // play player death sound
     playerDeathSound.play();
     noLoop();
 
-    bgMusic.stop();
-    spriteCrosshair = loadImage();
-    cursor(ARROW);
+    bgMusic.stop(); // stop background music
+    spriteCrosshair = loadImage();  // remove crosshair image
+    cursor(ARROW);  // show cursor
     
     push();
     shadow('rgba(0, 0, 0, 1)');
@@ -182,30 +201,38 @@ function draw() {
   spriteCrosshair.delay(5);
 }
 
+// Handle key presses
 function keyPressed() {
+  // Start game on any key press except ESCAPE and F1
   if (keyCode !== ESCAPE && keyCode !== 112 && showText) {
     showText = false;
     showReminder = true;
   }
+  // Restart game on 'R' key press if dead
   if ((key === "r" || key === "R") && death === true) {
     resetGame();
   }
+  // Pause/unpause game on ESCAPE key press
   if (keyCode === ESCAPE) {
     pauseGame();
   }
+  // Spawn health pack on 'Y' key press (debugging)
   if (key === "y" || key === "Y") {
     smallHealthPacks.push(new SmallHealthPack(hpX, hpY));
   }
+  // Start wave on ENTER/RETURN key press
   if (keyCode === ENTER && !powerUpScreen) {
     start = true;
     showReminder = false;
   }
+  // Load enemy sprite on F1 key press (debugging)
   if (keyCode === 112) {
     spriteEnemy = loadImage('libraries/enemy.png');
     logo = loadImage('libraries/enemy.png');
   }
 }
 
+// Display start menu
 function startMenu() {
   let logoPosX = width/2-80;
   let logoPosY = 300*(height/1080);
@@ -238,6 +265,7 @@ function startMenu() {
     pop();
   }
 
+  // reminder to start wave
   if(showReminder) {
     push();
     shadow('rgba(0, 0, 0, 1)');
@@ -250,8 +278,10 @@ function startMenu() {
   }
 }
 
+// Play background music if not in text display mode
 function backgroundMusicPlay() {
   if (!showText) {
+    // play background music if not already playing
     if (!bgMusic.isPlaying()) {
       bgMusic.setVolume(bgVolume);
       bgMusic.play();
@@ -259,6 +289,7 @@ function backgroundMusicPlay() {
   }
 }
 
+// Handle window resizing
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   
@@ -278,6 +309,7 @@ function windowResized() {
   pauseGame();
 }
 
+// Pause and unpause the game
 function pauseGame() {
   if (!paused && !showText && !death) {
     frameRate(0); // freeze the game
@@ -301,8 +333,9 @@ function pauseGame() {
   paused = !paused;
 }
 
+// Reset the game
 function resetGame() {
-  window.location.reload();
+  window.location.reload(); // goofy way to reset the game, but idc it works
   /*
   player.maxHealth = 100;
   player.health = 100;
@@ -317,6 +350,7 @@ function resetGame() {
   enemyKills = 0;*/
 }
 
+// Set shadow properties for drawing context
 function shadow(color, blurRadius = 10, offsetX = 0, offsetY = 0) {
   drawingContext.shadowColor = color;
   drawingContext.shadowBlur = blurRadius;
@@ -324,15 +358,15 @@ function shadow(color, blurRadius = 10, offsetX = 0, offsetY = 0) {
   drawingContext.shadowOffsetY = offsetY;
 }
 
+// Handle mouse presses
 function mousePressed() { 
   if (powerUpScreen) {   
     //power up 1
-
     if (mouseX > (width/3)-100 && mouseX < (width/3)+100 && mouseY > (height/2)-(133.33/2) && mouseY < (height/2)+(133.33/2)) {
       powerUp(choice);
       powerUpScreen = false;
     }
-
+    //power up 2
     if (mouseX > (2*width/3)-100 && mouseX < (2*width/3)+100 && mouseY > (height/2)-(133.33/2) && mouseY < (height/2)+(133.33/2)) {
       powerUp(choice2);
       powerUpScreen = false;
@@ -366,22 +400,22 @@ function loadAudio() {
 
   // Preload sound effects
   soundFormats('mp3', 'ogg', 'wav');
-
+  // Preload shooting sound
   shootSound = loadSound('assets/sounds/Pew.wav');
   shootSound.setVolume(bulletVolume);
-
+  // Damage sound
   playerDamageSound = loadSound('assets/sounds/player_damage.wav');
   playerDamageSound.setVolume(sfxVolume);
-
+  // Health pack pickup sound
   healthPackSound = loadSound('assets/sounds/health_pickup.wav');
   healthPackSound.setVolume(sfxVolume);
-
+  // Player death sounds
   playerDeathSound = loadSound('assets/sounds/player_death.wav');
   playerDeathSound.setVolume(sfxVolume);
-
+  // Enemy death sound
   enemyDeathSound = loadSound('assets/sounds/enemy_death.wav');
   enemyDeathSound.setVolume(sfxVolume);
-
+  // Boss hit sound
   bossHitSound = loadSound('assets/sounds/boss_hit.mp3');
   bossHitSound.setVolume(sfxVolume);
 
@@ -393,8 +427,11 @@ function loadAudio() {
   bgMusic.loop();
 }
 
+// Update and display all copters in the given array
 function copterArrUpdate(arr) {
+  // Iterate through the array of copters
   if (arr.length > 0) {
+    // Update, display, and check collision for each copter
     for (i = 0; i < arr.length; i++) { 
       const object = arr[i];
       object.update();  
@@ -404,8 +441,11 @@ function copterArrUpdate(arr) {
   }
 }
 
+// Update and display all bullets in the given array
 function bulletArrUpdate(arr) {
+  // Iterate through the array of bullets
   if (arr.length > 0) {
+    // Update and display each bullet
     for (i = 0; i < arr.length; i++) { 
       const object = arr[i];
       object.update();  
